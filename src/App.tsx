2bfe15ed1b6d2b2106f3468fd6d7f70a4cfdb75f
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 20:23:17 -0700

    Fetch and transform data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
-function App() {
-  return <h1>Data Fetching!</h1>;
+import { ReactNode, useEffect, useState } from 'react'
+
+import BlogPosts, { BlogPost } from './components/BlogPosts.tsx'
+import { get } from './util/http.ts'
+import fetchingImage from './assets/data-fetching.png'
+
+type RawDataBlogPost = {
+  id: number
+  userId: number
+  title: string
+  body: string
 }
 
-export default App;
+function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>()
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )) as RawDataBlogPost[]
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        }
+      })
+
+      setFetchedPosts(blogPosts)
+    }
+
+    fetchPosts()
+
+    return () => {}
+  }, [])
+
+  let content: ReactNode
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />
+  }
+
+  return (
+    <main>
+      <img
+        src={fetchingImage}
+        alt="An abstract image depicting a data fetching process"
+      />
+      {content}
+    </main>
+  )
+}
+
+export default App

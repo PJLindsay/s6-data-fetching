77cf15e07d1d356bed9d64763e64ba7518e3e7fd
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 20:32:57 -0700

    Refactor to use zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import { ReactNode, useEffect, useState } from 'react'
 
+import { z } from 'zod'
+
+// outside of App component function (since this doesn't need to be re-created all the time)
+const rawDataBlogPostSchema = z.object({
+  id: z.number(),
+  userId: z.number(),
+  title: z.string(),
+  body: z.string(),
+})
+
+const expectedResponseDataSchema = z.array(rawDataBlogPostSchema)
+
 import BlogPosts, { BlogPost } from './components/BlogPosts.tsx'
 import { get } from './util/http.ts'
 import fetchingImage from './assets/data-fetching.png'
@@ -13,30 +25,44 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>()
+  const [error, setError] = useState<string>()
+  const [isFetching, setIsFetching] = useState<boolean>(false)
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get(
-        'https://jsonplaceholder.typicode.com/posts'
-      )) as RawDataBlogPost[]
-
-      const blogPosts: BlogPost[] = data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
+      setIsFetching(true)
+      try {
+        const data = await get('https://jsonplaceholder.typicode.com/posts')
+        const parsedData = expectedResponseDataSchema.parse(data)
+        // No more type casting via "as" needed!
+        // Instead, here, TypeScript "knows" that parsedData will be an array
+        // full with objects as defined by the above schema
+        const blogPosts: BlogPost[] = parsedData.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          }
+        })
+        setFetchedPosts(blogPosts)
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message)
         }
-      })
+        // setError('Failed to fetch posts!');
+      }
 
-      setFetchedPosts(blogPosts)
+      setIsFetching(false)
     }
 
     fetchPosts()
-
-    return () => {}
   }, [])
 
   let content: ReactNode
+  if (isFetching) {
+    content = <p>Loading...</p>
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />
   }
